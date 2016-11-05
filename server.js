f24ecfca9ae665ed82f7025ab2c71b19a274b1e7
rémi gidon <gidonremi42@gmail.com>
2016-11-05 19:02:12 +0100

    fix server.js return index.html file now

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index_test.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 
