c84b9235abd2bb7cbe36c43fbcd0865ba8d169ee
rémi gidon <gidonremi42@gmail.com>
2016-11-30 18:31:03 +0100

    webpack works 2

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+
 
 
 Array.prototype.indexOfObj || (Array.prototype.indexOfObj = function(prop, value) {
